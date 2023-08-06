05b7bb4d7d4f2d3ead3c850a0c8f02edd3f7dbbc
MOHAMMAD HANZALA KHAN <hanzalakhan409@gmail.com>
2023-08-06 19:41:26 +0530

    filter added added

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -5,9 +5,9 @@ import LineChart from './LineChart';
 import PieChart from './PieChart';
 import DashboardStats from './DashboardStats';
 import UserGroupIcon from '@heroicons/react/24/outline/UserGroupIcon';
-import UsersIcon  from '@heroicons/react/24/outline/UsersIcon'
-import CircleStackIcon  from '@heroicons/react/24/outline/CircleStackIcon'
-import CreditCardIcon  from '@heroicons/react/24/outline/CreditCardIcon'
+import UsersIcon from '@heroicons/react/24/outline/UsersIcon'
+import CircleStackIcon from '@heroicons/react/24/outline/CircleStackIcon'
+import CreditCardIcon from '@heroicons/react/24/outline/CreditCardIcon'
 
 
 function Dashboard(props) {
@@ -66,55 +66,178 @@ function Dashboard(props) {
     })
   }, []);
 
- 
 
-const removerDublicateObj=(arr)=>{
-  const names = arr.map(({ name }) => name);
-  const filtered = arr.filter(({ name }, index) => !names.includes(name, index + 1))
-  return filtered
+
+  const removerDublicateObj = (arr) => {
+    const names = arr.map(({ name }) => name);
+    const filtered = arr.filter(({ name }, index) => !names.includes(name, index + 1))
+    return filtered
+  }
+
+
+
+  const [endYearFilter, setEndYearFilter] = useState(null);
+  const [topicFilter, setTopicFilter] = useState(null);
+  const [sectorFilter, setSectorFilter] = useState(null);
+  const [regionFilter, setRegionFilter] = useState(null);
+  const [pestFilter, setPestFilter] = useState(null);
+  const [sourceFilter, setSourceFilter] = useState(null);
+  const [countryFilter, setCountryFilter] = useState(null);
+  // ... and so on for each filter
+
+const resetAllFilter=()=>{
+  setEndYearFilter(null);
+  setTopicFilter(null);
+  setSectorFilter(null);
+  setRegionFilter(null);
+  setPestFilter(null);
+  setSourceFilter(null);
+  setCountryFilter(null);
+
 }
 
-  const sourceIntensityData  = hardData.map(item => ({ name: item.topic, value1: item.end_year,value2:item.end_year,value3: item.country,value4:item.insight })).slice(0, 10);
-  const sourceLikelihoodData = removerDublicateObj(hardData.map(item => ({ name: item.source, value1: item.topic,value2:item.region,value3: item.relevance,value4:item.insight })).slice(0,10));
-  const sourceRelevanceData = removerDublicateObj(hardData.map(item => ({ name: item.topic , value1: item.intensity,value2:item.likelihood,value3: item.relevance, }))).slice(0,10);
-  const startYearIntensityData = hardData.map(item => ({ name: item.start_year, value: item.intensity })).slice(0, 10);
- 
+  const filterData = (data) => {
+    let filtered = [...data];
+
+    if (endYearFilter) filtered = filtered.filter(item => item.end_year === endYearFilter);
+    if (topicFilter) filtered = filtered.filter(item => item.topic === topicFilter);
+    if (sectorFilter) filtered = filtered.filter(item => item.sector === sectorFilter);
+    if (regionFilter) filtered = filtered.filter(item => item.region === regionFilter);
+    if (pestFilter) filtered = filtered.filter(item => item.pestle === pestFilter);
+    if (sourceFilter) filtered = filtered.filter(item => item.source === sourceFilter);
+    // ... additional filters as needed
+
+    return filtered;
+  }
+
+  const uniqueTopics = [...new Set(hardData.map(item => item.topic))];
+  const uniqueEndYears = [...new Set(hardData.map(item => item.end_year))];
+  const uniqueSectors = [...new Set(hardData.map(item => item.sector))];
+  const uniqueRegions = [...new Set(hardData.map(item => item.region))];
+  const uniquePESTs = [...new Set(hardData.map(item => item.pestle))];
+  const uniqueSources = [...new Set(hardData.map(item => item.source))];
+  const uniqueCountries = [...new Set(hardData.map(item => item.country))];
+
+
+  const filteredData = filterData(hardData);
+
+
+  // const sourceIntensityData = hardData.map(item => ({ name: item.topic, value1: item.end_year, value2: item.end_year, value3: item.country, value4: item.insight })).slice(0, 15);
+  const sourceLikelihoodData = removerDublicateObj(filteredData.map(item => ({ name: item.source, value1: item.topic, value2: item.region, value3: item.relevance, value4: item.insight })));
+  const sourceRelevanceData = removerDublicateObj(filteredData.map(item => ({ name: item.topic, value1: item.intensity, value2: item.likelihood, value3: item.relevance, }))).slice(0, 15);
+  const startYearIntensityData = filteredData.map(item => ({ name: item.start_year, value: item.intensity })).slice(0, 15);
+
 
   const statsData = [
-    {title : "New Users", value : "34.7k", icon : <UserGroupIcon className='w-8 h-8'/>, description : "↗︎ 2300 (22%)"},
-    {title : "Total Sales", value : "$34,545", icon : <CreditCardIcon className='w-8 h-8'/>, description : "Current month"},
-    {title : "Pending Leads", value : "450", icon : <CircleStackIcon className='w-8 h-8'/>, description : "50 in hot leads"},
-    {title : "Active Users", value : "5.6k", icon : <UsersIcon className='w-8 h-8'/>, description : "↙ 300 (18%)"},
-]
-
+    { title: "New Users", value: "34.7k", icon: <UserGroupIcon className='w-8 h-8' />, description: "↗︎ 2300 (22%)" },
+    { title: "Total Sales", value: "$34,545", icon: <CreditCardIcon className='w-8 h-8' />, description: "Current month" },
+    { title: "Pending Leads", value: "450", icon: <CircleStackIcon className='w-8 h-8' />, description: "50 in hot leads" },
+    { title: "Active Users", value: "5.6k", icon: <UsersIcon className='w-8 h-8' />, description: "↙ 300 (18%)" },
+  ]
 
   return (
     <>
 
-      <div className={`grid grid-cols-1 gap-8 w-full h-auto mt-16 overflow-auto ${open? 'ml-72':'ml-20'} bg-[#f2f2f2] `}>
-          {/** ---------------------- Different stats content 1 ------------------------- */}
-          <div className="grid lg:grid-cols-4 mt-2 md:grid-cols-2 grid-cols-1 gap-6 mx-6">
-                {
-                    statsData.map((d, k) => {
-                        return (
-                            <DashboardStats key={k} {...d} colorIndex={k}/>
-                        )
-                    })
-                }
-            </div>
+      <div className={`grid grid-cols-1 gap-8 w-full h-auto mt-16 overflow-auto ${open ? 'ml-72' : 'ml-20'} bg-[#f2f2f2] `}>
+
+
+        <div className="filter-section">
+
+          {/* End Year Filter */}
+          <select value={endYearFilter} onChange={e => setEndYearFilter(e.target.value)}>
+            <option value="">All End Years</option>
+            {uniqueEndYears.map(year => {
+              if (year !== "") {
+                return <option key={year} value={year}>{year}</option>
+              }
+            })}
+          </select>
+
+          {/* Topic Filter */}
+          <select value={topicFilter} onChange={e => setTopicFilter(e.target.value)}>
+            <option value="">All Topics</option>
+            {uniqueTopics.map(topic => {
+              if (topic !== "") {
+                return <option key={topic} value={topic}>{topic}</option>
+              }
+            })}
+          </select>
+
+          {/* Sector Filter */}
+          <select value={sectorFilter} onChange={e => setSectorFilter(e.target.value)}>
+            <option value="">All Sectors</option>
+            {uniqueSectors.map(sector => {
+              if (sector !== "") {
+                return <option key={sector} value={sector}>{sector}</option>
+              }
+            })}
+          </select>
+
+          {/* Region Filter */}
+          <select value={regionFilter} onChange={e => setRegionFilter(e.target.value)}>
+            <option value="">All Regions</option>
+            {uniqueRegions.map(region => {
+              if (region !== "") {
+                return <option key={region} value={region}>{region}</option>
+              }
+            })}
+          </select>
+
+          {/* PEST Filter */}
+          <select value={pestFilter} onChange={e => setPESTFilter(e.target.value)}>
+            <option value="">All PESTs</option>
+            {uniquePESTs.map(pest => {
+              if (pest !== "") {
+                return <option key={pest} value={pest}>{pest}</option>
+              }
+            })}
+          </select>
+
+          {/* Source Filter */}
+          <select value={sourceFilter} onChange={e => setSourceFilter(e.target.value)}>
+            <option value="">All Sources</option>
+            {uniqueSources.map(source => {
+              if (source !== "") {
+                return <option key={source} value={source}>{source}</option>
+              }
+            })}
+          </select>
+
+          {/* Country Filter */}
+          <select value={countryFilter} onChange={e => setCountryFilter(e.target.value)}>
+            <option value="">All Countries</option>
+            {uniqueCountries.map(country => {
+              if (country !== "") {
+                return <option key={country} value={country}>{country}</option>
+              }
+            })}
+          </select>
+          <button onClick={resetAllFilter}></button>
+        </div>
+
+        {/** ---------------------- Different stats content 1 ------------------------- */}
+        <div className="grid lg:grid-cols-4 mt-2 md:grid-cols-2 grid-cols-1 gap-6 mx-6">
+          {
+            statsData.map((d, k) => {
+              return (
+                <DashboardStats key={k} {...d} colorIndex={k} />
+              )
+            })
+          }
+        </div>
         <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden bg-[#fffff]">
           <LineChart data={sourceRelevanceData} />
         </div>
         <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden bg-[#fffff]">
           <BarChart data={sourceLikelihoodData} />
-          
+
         </div>
         <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden bg-[#fffff]">
           <PieChart data={startYearIntensityData} className="ml-8" />
         </div>
-      </div>
+      </div >
       <div>
-      
+
       </div>
 
     </>
