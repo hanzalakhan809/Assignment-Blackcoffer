0aee444527c1966fe6827eb288e3be11b539788c
MOHAMMAD HANZALA KHAN <hanzalakhan409@gmail.com>
2023-08-06 14:47:08 +0530

    dashboard top cards added

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,7 +3,11 @@ import getChartData from '../../services/data';
 import BarChart from './BarChart';
 import LineChart from './LineChart';
 import PieChart from './PieChart';
-
+import DashboardStats from './DashboardStats';
+import UserGroupIcon from '@heroicons/react/24/outline/UserGroupIcon';
+import UsersIcon  from '@heroicons/react/24/outline/UsersIcon'
+import CircleStackIcon  from '@heroicons/react/24/outline/CircleStackIcon'
+import CreditCardIcon  from '@heroicons/react/24/outline/CreditCardIcon'
 
 
 function Dashboard(props) {
@@ -76,17 +80,36 @@ const removerDublicateObj=(arr)=>{
   const startYearIntensityData = hardData.map(item => ({ name: item.start_year, value: item.intensity })).slice(0, 10);
  
 
+  const statsData = [
+    {title : "New Users", value : "34.7k", icon : <UserGroupIcon className='w-8 h-8'/>, description : "↗︎ 2300 (22%)"},
+    {title : "Total Sales", value : "$34,545", icon : <CreditCardIcon className='w-8 h-8'/>, description : "Current month"},
+    {title : "Pending Leads", value : "450", icon : <CircleStackIcon className='w-8 h-8'/>, description : "50 in hot leads"},
+    {title : "Active Users", value : "5.6k", icon : <UsersIcon className='w-8 h-8'/>, description : "↙ 300 (18%)"},
+]
+
+
   return (
     <>
-      <div className={`grid grid-cols-1 gap-8 w-full h-auto mt-16 overflow-auto ${open? 'ml-72':'ml-20'} `}>
-        <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden">
+
+      <div className={`grid grid-cols-1 gap-8 w-full h-auto mt-16 overflow-auto ${open? 'ml-72':'ml-20'} bg-[#f2f2f2] `}>
+          {/** ---------------------- Different stats content 1 ------------------------- */}
+          <div className="grid lg:grid-cols-4 mt-2 md:grid-cols-2 grid-cols-1 gap-6 mx-6">
+                {
+                    statsData.map((d, k) => {
+                        return (
+                            <DashboardStats key={k} {...d} colorIndex={k}/>
+                        )
+                    })
+                }
+            </div>
+        <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden bg-[#fffff]">
           <LineChart data={sourceRelevanceData} />
         </div>
-        <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden">
+        <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden bg-[#fffff]">
           <BarChart data={sourceLikelihoodData} />
           
         </div>
-        <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden">
+        <div className="col-span-2 shadow-xl rounded-xl border mx-6 p-4 flex justify-center overflow-hidden bg-[#fffff]">
           <PieChart data={startYearIntensityData} className="ml-8" />
         </div>
       </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,36 @@
+// module.exports = {
+  //   mode: "jit",
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         "dark-purple": "#081A51",
+//         "light-white": "rgba(255,255,255,0.17)",
+//       },
+//     },
+//   },
+// plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+
+// };
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+  ],
+  darkMode: ["class", '[data-theme="dark"]'],
   theme: {
     extend: {
       colors: {
-        "dark-purple": "#081A51",
-        "light-white": "rgba(255,255,255,0.17)",
-      },
+                "dark-purple": "#081A51",
+                "light-white": "rgba(255,255,255,0.17)",
+              },
     },
   },
-  plugins: [],
-};
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark",],
+  },
+
+}
